3b778a49d04a76fa039149f131894098e11ab738
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-22 20:53:00 +0700

    schema modelling

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => res.send('Hello Ais!'));
+app.get('/ejs', (req,res)=> res.render('index'));
 app.get('/:name', (req,res) => res.send(`nama saya: ${req.params.name}`));
 
 app.listen(port, ()=> console.log(`Example app listening at http://localhost:${port}`));
