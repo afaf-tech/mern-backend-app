66f13a0adcc30f74fba4f60396c5e1df7c412a49
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-25 09:33:52 +0700

    category operations

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+//import mongoose
+const mongoose =require('mongoose');
+
+mongoose.connect('mongodb://173.82.243.249:27017/db_staycation', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +23,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
 
 app.use(logger('dev'));
 app.use(express.json());
