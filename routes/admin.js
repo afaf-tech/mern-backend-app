a1ec3067d1fcbef9d7f1a7937bc0d7310ab2d423
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-27 10:58:53 +0700

    booking admin side inteface

--- routes/admin.js
+++ routes/admin.js
@@ -36,6 +36,9 @@ router.delete('/item/:itemId/activity/:id', adminController.deleteActivity);
 
 
 router.get('/booking', adminController.viewBooking);
+router.get('/booking/:id', adminController.showDetailBooking);
+router.put('/booking/:id/confirmation', adminController.actionConfirmation);
+router.put('/booking/:id/reject', adminController.actionReject);
 
 
 
