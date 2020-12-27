329ad1645f459e5088e5c91c67e4632587d232ec
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-27 19:42:33 +0700

    book api

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const apiController = require('../controllers/apiController.js');
 const auth = require('../middlewares/auth');
 // const {upload } = require('../middlewares/multer');
-const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
+const { uploadSingle } = require('../middlewares/multer');
 router.get('/landing_page', apiController.landing_page);
 router.get('/detail-page/:id', apiController.detailPage);
+router.post('/booking-page', uploadSingle, apiController.bookingPage);
 
 
 module.exports = router;
