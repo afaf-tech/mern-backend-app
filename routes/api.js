a16d70e11720205619620d056a88a71951bf296a
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-27 18:03:36 +0700

    detail page api

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,8 @@ const apiController = require('../controllers/apiController.js');
 const auth = require('../middlewares/auth');
 // const {upload } = require('../middlewares/multer');
 const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
-router.get('/member/landing_page', apiController.landing_page);
+router.get('/landing_page', apiController.landing_page);
+router.get('/detail-page/:id', apiController.detailPage);
 
 
 module.exports = router;
